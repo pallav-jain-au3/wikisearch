64eee619c4a25ed905df2d91cb98ead1748b0ff5
Pallav Jain <pallavjain409@gmail.com>
2019-05-01 10:25:06 +0530

    third commit

--- index.js
+++ index.js
@@ -1,40 +1,45 @@
-function userInput()
-{
-  html = "";
-  let userInput = this.value;
+function checkForUserInput() {
+  const searchInput = document.querySelector('.search');
+  searchInput.addEventListener('change', userInputAndHtml);
+  searchInput.addEventListener('keyup', userInputAndHtml);
+}
+
+function userInputAndHtml() {
+  if (this.value) {
+    html = "";
+    userInput(this.value);
+  }
+  document.querySelector('.articles').innerHTML = "";
+}
+
+function userInput(value) {
+
+  let userInput = value;
   let wikiAPI = "https://en.wikipedia.org/w/api.php?action=opensearch&&search=" + userInput;
   getDataFromAPI(wikiAPI);
 }
-function randomInfo(){
-  let randomAPI = "https://en.wikipedia.org/wiki/Special:Random";
-  let button = document.querySelector('.btn');
-  button.addEventListener('click', getDataFromAPI(randomAPI) );
-}
-function getDataFromAPI(url){
-  $.get(url, function(data){
+
+function getDataFromAPI(url) {
+  $.get(url, function(data) {
     displayArticles(data);
   }, 'jsonp');
 }
-function displayArticles(data){
+
+function displayArticles(data) {
   let heading = data[1];
   let articleInfo = data[2];
   let wikiLink = data[3];
-  for (let index in heading){
+  for (let index in heading) {
     displayInHtml(heading[index], articleInfo[index], wikiLink[index]);
   }
 }
 
-function displayInHtml(heading, aboutArticle, link){
+function displayInHtml(heading, aboutArticle, link) {
   html += `<li><a href = ${link} target="_blank"><h1>${heading}</h1>
   <p>${aboutArticle}</p></li>`;
   document.querySelector('.articles').innerHTML = html;
 }
-function checkForUserInput(){
-  const searchInput = document.querySelector('.search');
-  searchInput.addEventListener('change',userInput);
-  searchInput.addEventListener('keyup',userInput);
-}
+
 
 checkForUserInput();
-randomInfo();
 let html = "";
